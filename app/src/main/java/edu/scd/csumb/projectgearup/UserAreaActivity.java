eb046264a5ac4e3b56af0e1ef73aaab726d287e5
Sam <savaldez@csumb.edu>
2017-12-03 20:10:52 -0800

    IT WORKS! SENDS DATA FROM PHONE APP TO DATABASE! 22 hours!

--- app/src/main/java/edu/scd/csumb/projectgearup/UserAreaActivity.java
+++ app/src/main/java/edu/scd/csumb/projectgearup/UserAreaActivity.java
@@ -1,11 +1,17 @@
 package edu.scd.csumb.projectgearup;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -17,13 +23,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.apache.http.params.HttpParams;
 
 import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
@@ -34,13 +50,19 @@ import java.net.URLEncoder;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class UserAreaActivity extends AppCompatActivity implements View.OnClickListener{
+import javax.net.ssl.HttpsURLConnection;
+
+public class UserAreaActivity extends AppCompatActivity {
     private static final int RESULT_LOAD_IMAGE = 1;
-    ImageView imageToUpload;
-    Button bUploadImage;
+    ImageView imageToUpload, imageUpload;
+    Button bUploadImage, btnChoose, btnUpload;
     EditText uploadImageName;
-
+    final int CODE_GALLERY_REQUEST = 999;
+    String urlUpload = "https://obtuse-angular-over.000webhostapp.com/upload.php";
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,117 +81,106 @@ public class UserAreaActivity extends AppCompatActivity implements View.OnClickL
 
         welcomeMess.setText(message);
         etUsername.setText(username);
+        btnChoose = (Button) findViewById(R.id.btnChoose);
+        btnUpload = (Button) findViewById(R.id.bUploadImage);
+        imageUpload = (ImageView) findViewById(R.id.imageUpload);
 
-        imageToUpload = (ImageView) findViewById(R.id.imageToUpload) ;
-        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        btnChoose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                ActivityCompat.requestPermissions(
+                        UserAreaActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        CODE_GALLERY_REQUEST
 
-        uploadImageName = (EditText) findViewById(R.id.etUploadName);
-        imageToUpload.setOnClickListener(this);
-        bUploadImage.setOnClickListener(this);
+                );
+            }
+        });
+
+       btnUpload.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               StringRequest stringRequest = new StringRequest(Request.Method.POST, urlUpload, new Response.Listener<String>() {
+                   @Override
+                   public void onResponse(String response) {
+                        Toast.makeText(getApplication(),response,Toast.LENGTH_LONG).show();
+
+                   }
+
+               }, new Response.ErrorListener() {
+                   @Override
+                   public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(getApplication(),"Error: " + volleyError.toString(), Toast.LENGTH_LONG).show();
+                   }
+               }){
+                   @Override
+                   protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> params = new HashMap<>();
+                            String imageData = imageToString(bitmap);
+                            params.put("image", imageData);
+
+                       return params;
+                   }
+               };
+               RequestQueue requestQueue = Volley.newRequestQueue(UserAreaActivity.this);
+               requestQueue.add(stringRequest);
+           }
+
+
+       });
+//        imageToUpload = (ImageView) findViewById(R.id.imageToUpload) ;
+//        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+//
+//        uploadImageName = (EditText) findViewById(R.id.etUploadName);
+//        imageToUpload.setOnClickListener(this);
+//        bUploadImage.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
-    switch (view.getId()){
-        case R.id.imageToUpload:
-            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent,RESULT_LOAD_IMAGE);
-            break;
-        case R.id.bUploadImage:
-            Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
-            new UploadImage(image, uploadImageName.getText().toString()).execute();
-            break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,@NonNull int[] grantResults){
 
-    }
+        if(requestCode == CODE_GALLERY_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Image"), CODE_GALLERY_REQUEST);
+
+            } else {
+                Toast.makeText(getApplicationContext(), "YOU DONT HAAVE ACCESS TO GALLERY", Toast.LENGTH_LONG).show();
+            }
+            return;
+
+        }
+
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == CODE_GALLERY_REQUEST && resultCode == RESULT_OK && data != null){
+           Uri filePath = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(filePath);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                imageUpload.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
-            Uri selectedImage = data.getData();
-            imageToUpload.setImageURI(selectedImage);
-
-        }
     }
 
+    private String imageToString(Bitmap bitmap){
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100,outputStream);
+        byte[] imageBytes = outputStream.toByteArray();
 
-    private class UploadImage extends AsyncTask<Void,Void, Void>{
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
 
-        Bitmap image;
-        String name;
-        public UploadImage(Bitmap image, String name){
-            this.image = image;
-            this.name = name;
-        }
-        @Override
-        protected Void doInBackground(Void... voids) {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-
-
-//            ArrayList<Pair> dataToSend = new ArrayList<Pair>();
-//            Pair pair = new Pair();
-//            pair.setX("image");
-//            pair.setY(encodedImage);
-//            dataToSend.add(pair);
-//
-//            pair.setX("name");
-//            pair.setY(name);
-//            dataToSend.add(pair);
-
-            String urlParameters  =   "image="+ encodedImage + "&name="+ name;
-
-
-            byte[] postData = urlParameters.getBytes( Charset.forName("UTF-8") );
-            int postDataLength = postData.length;
-            String request = "http://obtuse-angular-over.000webhostapp.com/SavePicture.php";
-            URL url = null;
-            try {
-                url = new URL( request );
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            HttpURLConnection conn= null;
-            try {
-                conn = (HttpURLConnection) url.openConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            conn.setDoOutput(true);
-            conn.setInstanceFollowRedirects(false);
-            try {
-                conn.setRequestMethod("POST");
-            } catch (ProtocolException e) {
-                e.printStackTrace();
-            }
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            conn.setRequestProperty("charset", "utf-8");
-            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength ));
-            conn.setUseCaches(false);
-
-            DataOutputStream wr = null;
-            try {
-                wr =  new DataOutputStream(conn.getOutputStream());
-                wr.write( postData );
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
-        }
-
-
+        return encodedImage;
     }
-
 }
